7d0a4b6226616f1a07809c69f354677c29fb3c72
Kyle Beasley <kyle.beasley17@gmail.com>
2025-01-14 14:57:10 -0600

    basic login, home, and registration routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', function (Request $request) : RedirectResponse|View {
+    if ($request->hasCookie('user')) {
+        // validate the user cookie then send to the home page
+        return view('home');
+    }
+
+    return redirect('/login');
+});
+
+Route::get('/login', function (Request $request) : View {
+   return view('login');
+});
+
+Route::get('/register', function (Request $request) : View {
+   return view('register');
 });
