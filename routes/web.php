62c026c510d697ff07a4d952baf12fb4082be17e
Kyle Beasley <kyle.beasley17@gmail.com>
2025-01-15 11:28:26 -0600

    ready to test out registration flow

--- routes/web.php
+++ routes/web.php
@@ -6,18 +6,33 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function (Request $request) : RedirectResponse|View {
-    if ($request->hasCookie('user')) {
-        // validate the user cookie then send to the home page
-        return view('home');
+    if ($request->session()->has('registered-user')) {
+        // send to dashboard with newly registered user
+        $user = $request->session()->get('registered-user');
+
+        return redirect()->route('dashboard')->with('user', $user);
+    } else if ($request->hasCookie('auth-cookie')) {
+        $userCookie = $request->cookie('auth-cookie');
+
+        // validate the cookie and send to the dashboard
+        return redirect()->route('dashboard')->with('user', $userCookie);
     }
 
     return redirect('/login');
+})->name('home');
+
+Route::controller(\App\Http\Controllers\AuthController::class)->group(function () {
+    Route::get('/login', 'viewLogin')->name('login');
+    Route::post('/login', 'tryLogin');
+    Route::get('/register', 'viewRegister')->name('register');
+    Route::post('/register', 'tryRegister');
 });
 
-Route::get('/login', function (Request $request) : View {
-   return view('login');
-});
+Route::get('/dashboard', function(Request $request) : View|RedirectResponse
+{
+    if ($request->session()->has('user')) {
+        return view('dashboard');
+    }
 
-Route::get('/register', function (Request $request) : View {
-   return view('register');
-});
+    return redirect('/login');
+})->name('dashboard');
